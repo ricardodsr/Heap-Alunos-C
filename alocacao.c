10309bef579e5312ab9e71abf51a915755ee3dab
Ricardo Rouco <ricardo.rouco@gmail.com>
2023-03-03 10:38:50 +0000

    Update alocacao.c

--- alocacao.c
+++ alocacao.c
@@ -1,5 +1,27 @@
 #include "tp.h"
+/**
 
+// printColocados()
+// This function takes two parameters - an array of Colocado objects and an array of integers.
+// It prints out the code of the UCE (Unidade de Educação Continuada) and the ID, average score and preference level of each student placed in one of those UCEs.
+
+// ha_vagas()
+// This function takes three parameters - the index of the UCE, an array of Colocado objects, and an array of integers.
+// It checks if there are still vacancies in a particular UCE and returns a boolean value.
+
+// initlic()
+// This function takes one parameter - an array of Colocado objects. 
+// It initializes the Colocado array with zeros.
+
+// coloca()
+// This function takes four parameters - a pointer to an Aluno object, a pointer to a Preferencia object, an array of Colocado objects, and an array of integers. 
+// It assigns a student to an UCE based on their preference level and updates the Colocado array accordingly.
+
+// alocacao() 
+// This function takes four parameters - a HeapAlunos structure, an array of Colocado objects, an array of integers, and an array of UCE objects.
+// It handles the entire student placement process. It looks through the HeapAlunos structure and assigns students to UCEs based on their preferences. It also updates the Colocado array and the location array as it goes through the process
+
+**/
 
 int printColocados (Colocado lic[], int loc[]) {
    int i,j;
